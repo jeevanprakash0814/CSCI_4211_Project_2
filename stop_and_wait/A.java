9f2efefeb4388e9bc9114f55036a50ce6c2d19aa
Jeevan Prakash <praka066@umn.edu>
2021-11-17 22:33:28 -0600

    All Code Works Flawlessly And Documented - All the code works flawlessly for both go-back-n and stop-and-wait given the good lamda values - All documentation completed for both protocols - Added NACK for go-back-n which is what solved the issue - Working through README

--- stop_and_wait/A.java
+++ stop_and_wait/A.java
@@ -6,38 +6,47 @@ public class A {
     
     public A(){
         // initialization of the states of A
-        this.seq = 0;
-        this.state = "WAIT_LAYER5";
-        this.estimated_rtt = 30;
+        this.seq = 0; // initialize the first sequence number
+        this.state = "WAIT_LAYER5"; // current state of A
+        this.estimated_rtt = 30; // estimated rtt time for each packet
     }
     public void A_input(simulator sim, packet p){
         // receive data from the other side
         // process the ACK, NACK from B
-        // what to do if the state is already WAIT_LAYER5
-        // state should be WAIT_ACK for this method
+
+        // ensure packet is not corrupted
         if(p.checksum != p.get_checksum());
-        else if(lastpacket.acknum != p.acknum) {
+        else if(lastpacket.acknum != p.acknum) { // incorrect ACK received
+            // restart timer
             sim.envlist.remove_timer();
             sim.envlist.start_timer('A',(float)estimated_rtt);
+            
+             // resend last packet
             sim.to_layer_three('A', lastpacket);
+
+            // reset state
             state = "WAIT_ACK";
         }
-        else {
+        else { // correct ACK received
+            // reset timer
             sim.envlist.remove_timer();
-            state = "WAIT_LAYER5"; // reset state
+            
+            // reset state
+            state = "WAIT_LAYER5";
         }
     }
     public void A_output(simulator sim, msg m){
         // called from layer 5, pass the data to the other side
+        
         // if state is still WAIT_ACK it means ACK has not come back so don't send new data
         if(state.equals("WAIT_ACK")) return;
         
-        // sets up new packet
+        // set up new packet
         packet p = new packet(this.seq, this.seq, m);
         this.seq = this.seq == 1 ? 0 : 1;
         lastpacket = new packet(p);
 
-        // reset timer
+        // start timer
         sim.envlist.start_timer('A',(float)estimated_rtt);
 
         // send packet to B
@@ -48,9 +57,11 @@ public class A {
     }
     public void A_handle_timer(simulator sim){
         // handler for time interrupt
+        
+        // restart timer
         sim.envlist.start_timer('A',(float)estimated_rtt);
         
-        // resend the packet as needed
+        // resend the packet
         sim.to_layer_three('A', lastpacket);
 
         // reset state
--- stop_and_wait/B.java
+++ stop_and_wait/B.java
@@ -3,22 +3,24 @@ public class B {
 
     public B(){
         // initialization of the state of B
-        this.seq = 0;
+        this.seq = 0; // initialize the first expected sequence number
     }
     public void B_input(simulator sim,packet pkt){
-        // TODO: process the packet recieved from the layer 3
+        // process the packet received from the layer 3
+
         // verify checksum
         if(pkt.checksum != pkt.get_checksum());
-        else if(pkt.seqnum != this.seq) {
-            // send ACK of previous packet to A
+        else if(pkt.seqnum != this.seq) { // incorrect packet received
+            // send NACK
             pkt.send_ack(sim, 'B', pkt.acknum);
-        }
-        else {
+        } else { // correct packet received
             // send payload to layer 5
             sim.to_layer_five('B',pkt.payload);
 
             // send ack of received packet to A
             pkt.send_ack(sim, 'B', pkt.acknum);
+
+            // alternate bit to for next expected sequence number
             this.seq = this.seq == 1 ? 0 : 1;
         }
     }
